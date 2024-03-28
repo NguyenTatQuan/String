8bb4648a806f1424330f04ee7b4eb3920fc77bff
kikon123 <tatquan2001@gmail.com>
2024-03-28 15:46:53 +0700

    Strinh TH

---
+++ src/Demo.java
@@ -0,0 +1,16 @@
+public class Demo {
+    public static void main(String[] args) {
+        String a = "quan";
+        String a1 = "quan";
+        a1 = "Quan dai ca";
+
+        User user = new User("Q",23);
+        User user1 = user;
+
+       StringBuilder a2 = new StringBuilder("Quat mat quan");
+       StringBuilder a3 = new StringBuilder("Q");
+        System.out.println(a3);
+
+
+    }
+}
